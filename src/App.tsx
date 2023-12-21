0c9eee94e497b4e2d05c63075edbde1f03bd7daa
Craig Ellenwood <73420772+inoculate23@users.noreply.github.com>
2023-12-20 22:17:15 -0800

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -6,8 +6,14 @@ function App() {
     const transcriber = useTranscriber();
 
     return (
+           <>
+     <img id="logo" src="https://inoculate.media/assets/logo_200.png" border="0px" width="200px" height="200px" />
+   
         <div className='flex justify-center items-center min-h-screen'>
             <div className='container flex flex-col justify-center items-center'>
+         <h2 className='mt-3 mb-5 px-4 text-center text-1xl font-semibold tracking-tight text-slate-900 sm:text-2xl'>
+           Haawke Neural Technology
+             </h2>
                 <h1 className='text-5xl font-extrabold tracking-tight text-slate-900 sm:text-7xl text-center'>
                     Whisper Web
                 </h1>
@@ -19,13 +25,12 @@ function App() {
             </div>
 
             <div className='absolute bottom-4'>
-                Made with{" "}
+               
                 <a
                     className='underline'
-                    href='https://github.com/xenova/transformers.js'
+                    href='mailto:info@inoculate.media'
                 >
-                    🤗 Transformers.js
-                </a>
+Contact us                </a>
             </div>
         </div>
     );
