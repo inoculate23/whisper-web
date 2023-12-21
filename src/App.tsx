e330afe5be1bac4575cf2013c6cf90ff6a424095
Craig Ellenwood <73420772+inoculate23@users.noreply.github.com>
2023-12-21 05:53:38 -0800

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -11,7 +11,7 @@ function App() {
    
         <div className='flex justify-center items-center min-h-screen'>
             <div className='container flex flex-col justify-center items-center'>
-                   <img src={logo} width="250px" alt="Logo" />;
+                   <img src={logo} width="290px" alt="Logo" />
 
         
                 <h1 className='text-5xl font-extrabold tracking-tight text-slate-900 sm:text-7xl text-center'>
